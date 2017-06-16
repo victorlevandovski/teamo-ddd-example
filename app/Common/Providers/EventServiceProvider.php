7ed8491376c42a73f52253de3f5bda6760594e4b
Victor Levandovski <victor@5mbytes.com>
2017-06-16 13:37:35 +0200

    Laravel application

---
+++ app/Common/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Teamo\Common\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'Teamo\Events\Event' => [
+            'Teamo\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
